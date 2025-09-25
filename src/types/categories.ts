f6061b9a86e81430109dddadf50ae35d232a4129
Father1993 <enjoyhillol@gmail.com>
2025-09-25 16:23:18 +1000

    add: new fn with categories products generate AI

---
+++ src/types/categories.ts
@@ -0,0 +1,17 @@
+export type Category = {
+    id: number
+    parent_id?: number | null
+    header?: string | null
+    sync_uid?: string | null
+    level?: number | null
+    product_count?: number | null
+    product_count_additional?: number | null
+    created_at?: string | null
+    updated_at?: string | null
+    description_added?: boolean | null
+    description?: string | null
+    push_to_pim?: boolean | null
+    description_confirmed?: boolean | null
+    confirmed_by_email?: string | null
+    is_rejected?: boolean | null
+}
